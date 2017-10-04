7bb71ebc11efb3c03439c05e0c21a3358fc1e2ae
Sam Gillingham <gillingham.sam@gmail.com>
2017-10-04 17:01:00 +1000

    added NaturalNeighbourPts

--- src/cgalinterp.cpp
+++ src/cgalinterp.cpp
@@ -94,7 +94,14 @@ static PyObject *cgalinterp_naturalneighbour(PyObject *self, PyObject *args)
         return NULL;
     }
     
-    // TODO: check types ok
+    // check types ok
+    if( (PyArray_TYPE(pXVals) != NPY_DOUBLE) || (PyArray_TYPE(pYVals) != NPY_DOUBLE) || 
+        (PyArray_TYPE(pZVals) != NPY_DOUBLE) || (PyArray_TYPE(pXGrid) != NPY_DOUBLE) ||
+        (PyArray_TYPE(pYGrid) != NPY_DOUBLE) )
+    {
+        PyErr_SetString(GETSTATE(self)->error, "All input arrays must be double");
+        return NULL;
+    }
 
     npy_intp nRows = PyArray_DIM(pXGrid, 0);
     npy_intp nCols = PyArray_DIM(pXGrid, 1);
@@ -206,6 +213,118 @@ static PyObject *cgalinterp_naturalneighbour(PyObject *self, PyObject *args)
     return (PyObject*)pOutArray;
 }    
 
+static PyObject *cgalinterp_naturalneighbour_pts(PyObject *self, PyObject *args)
+{
+    //std::cout.precision(12);
+    PyArrayObject *pXVals, *pYVals, *pZVals, *pXYGrid;
+    PyArrayObject *pOutArray;
+    npy_intp nPts, nVals, i;
+    
+    if( !PyArg_ParseTuple(args, "OOOO:NaturalNeighbourPts", &pXVals, &pYVals, &pZVals, &pXYGrid))
+        return NULL;
+    
+    if( !PyArray_Check(pXVals) || !PyArray_Check(pYVals) || !PyArray_Check(pZVals) || !PyArray_Check(pXYGrid) )
+    {
+        PyErr_SetString(GETSTATE(self)->error, "All arguments must be numpy arrays");
+        return NULL;
+    }
+
+    // check dims
+    if( (PyArray_NDIM(pXVals) != 1) || (PyArray_NDIM(pYVals) != 1) || (PyArray_NDIM(pZVals) != 1) || 
+            (PyArray_NDIM(pXYGrid) != 2) )
+    {
+        PyErr_SetString(GETSTATE(self)->error, "Arrays should be 1d, 1d, 1d and 2d respectively");
+        return NULL;
+    }
+    
+    // Check dimensions match
+    if( (PyArray_DIM(pXVals, 0) != PyArray_DIM(pYVals, 0)) | (PyArray_DIM(pXVals, 0) != PyArray_DIM(pZVals, 0)))
+    {
+        PyErr_SetString(GETSTATE(self)->error, "Training X, Y and Z arrays must all be of the same length");
+        return NULL;
+    }
+    
+    if( PyArray_DIM(pXYGrid, 1) != 2 )
+    {
+        PyErr_SetString(GETSTATE(self)->error, "Interpolation point array must be shape N*2");
+        return NULL;
+    }
+    
+    // check types ok
+    if( (PyArray_TYPE(pXVals) != NPY_DOUBLE) || (PyArray_TYPE(pYVals) != NPY_DOUBLE) || 
+        (PyArray_TYPE(pZVals) != NPY_DOUBLE) || (PyArray_TYPE(pXYGrid) != NPY_DOUBLE) )
+    {
+        PyErr_SetString(GETSTATE(self)->error, "All input arrays must be double");
+        return NULL;
+    }
+    
+    nPts = PyArray_DIM(pXYGrid, 0);
+    nVals = PyArray_DIM(pXVals, 0);
+    
+    // Create output
+    pOutArray = (PyArrayObject*)PyArray_EMPTY(1, &nPts, NPY_DOUBLE, 0);
+    if( pOutArray == NULL )
+    {
+        PyErr_SetString(GETSTATE(self)->error, "Failed to create array");
+        return NULL;
+    }
+    
+    if( PyArray_DIM(pXVals, 0) < 3 )
+    {
+        PyErr_SetString(GETSTATE(self)->error, "Not enough points, need at least 3.");
+        Py_DECREF(pOutArray);
+        return NULL;
+    }
+    
+    try
+    {
+        //std::cout << "Perform Interpolation\n";
+        DelaunayTriangulation *dt = new DelaunayTriangulation();
+        PointValueMap *values = new PointValueMap();
+        
+        //std::cout << "Building Triangulation\n";
+        for(i = 0; i < nVals; ++i)
+        {
+            K::Point_2 cgalPt( *((double*)PyArray_GETPTR1(pXVals, i)), *((double*)PyArray_GETPTR1(pYVals, i)) );
+            dt->insert(cgalPt);
+            CGALCoordType value = *((double*)PyArray_GETPTR1(pZVals, i));
+            //std::cout << i << "\t[" << *((double*)PyArray_GETPTR1(pXVals, i)) << ", " << *((double*)PyArray_GETPTR1(pYVals, i)) << "] = " << *((double*)PyArray_GETPTR1(pZVals, i)) << std::endl;
+            values->insert(std::make_pair(cgalPt, value));
+        }
+        for(i = 0; i < nPts; ++i)
+        {
+            K::Point_2 p( *((double*)PyArray_GETPTR2(pXYGrid, i, 0)), *((double*)PyArray_GETPTR2(pXYGrid, i, 1)) );
+            CoordinateVector coords;
+            CGAL::Triple<std::back_insert_iterator<CoordinateVector>, K::FT, bool> result = CGAL::natural_neighbor_coordinates_2(*dt, p, std::back_inserter(coords));
+            if(!result.third)
+            {
+                //std::cout << "\t Not within convex hull\n";
+                // Not within convex hull of dataset
+                *((double*)PyArray_GETPTR1(pOutArray, i)) = 0.0;
+            }
+            else
+            {
+                CGALCoordType norm = result.second;
+                CGALCoordType outValue = CGAL::linear_interpolation(coords.begin(), coords.end(), norm, CGAL::Data_access<PointValueMap>(*values));
+                //std::cout << "pt: [" << *((double*)PyArray_GETPTR2(pXGrid, i, j)) << ", " << *((double*)PyArray_GETPTR2(pYGrid, i, j)) << "]\n";
+                //std::cout << "\t NN Value = " << outValue << std::endl;
+                *((double*)PyArray_GETPTR1(pOutArray, i)) = outValue;
+            }
+        }
+            
+        delete dt;
+        delete values;
+    }
+    catch(std::exception &e)
+    {
+        PyErr_SetString(GETSTATE(self)->error, e.what());
+        return NULL;
+    }
+
+    return (PyObject*)pOutArray;
+}
+
+
 /* Our list of functions in this module*/
 static PyMethodDef CGALInterpMethods[] = {
     {"NaturalNeighbour", cgalinterp_naturalneighbour, METH_VARARGS,
@@ -219,6 +338,15 @@ static PyMethodDef CGALInterpMethods[] = {
 "  xgrid is a 2d array of x coordinates to interpolate at\n"
 "  ygrid is a 2d array of y coordinates to interpolate at\n"
 "xgrid and xgrid must be the same shape"}, 
+    {"NaturalNeighbourPts", cgalinterp_naturalneighbour_pts, METH_VARARGS,
+        "Perform Natural Neighbour Interpolation at points\n"
+        "call signature: arr = NaturalNeighbourPts(xvals, yvals, zvals, xygrid)\n"
+        "where:\n"
+        "  xvals is a 1d array of the x values of the points\n"
+        "  yvals is a 1d array of the y values of the points\n"
+        "  zvals is a 1d array of the z values of the points\n"
+        "xvals, yvals and zvals should have the same length\n"
+        "  xygrid is a 2d array of coordinates in the form x,y\n"},
     {NULL}        /* Sentinel */
 };
 
